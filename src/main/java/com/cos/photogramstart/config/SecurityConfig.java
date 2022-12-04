1a2a47e4d4b83f18252fb798212e18e016a1239d
kanggeol <xyritol@gamil.com>
2022-12-04 21:34:30 +0900

    챕터3까지 따라하기

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -1,21 +1,29 @@
 package com.cos.photogramstart.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.authorizeRequests().antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/auth/signin")
+                .loginPage("/auth/signin") //antMatchers에 포함된 주소로 접속하면 인증필요 -> /auth/signin 페이지 연결됨, get방식으로.
+                .loginProcessingUrl("/auth/signin") //auth/signin 페이지를 통해 로그인 시도되면 스프링 시큐리티가 로그인 프로세스 진행, post방식으로.
                 .defaultSuccessUrl("/");
-
     }
 }
--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,7 +1,65 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.service.AuthService;
+import com.cos.photogramstart.web.dto.auth.SignupDto;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
 @Controller
+
 public class AuthController {
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+    private final AuthService authService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/auth/signin")
+    public String signinForm() {
+        return "auth/signin";
+    }
+
+    @GetMapping("/auth/signup")
+    public String signupForm() {
+        return "auth/signup";
+    }
+
+    @PostMapping("/auth/signup")
+    public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) {
+//        log.info(signupDto.toString());
+// 입력받아 post로 넘어온 정보
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            throw new CustomValidationException("유효성 검사 실패", errorMap);
+        } else {
+            User user = signupDto.toEntity();
+//        log.info(user.toString());
+//User(id=0, username=444, password=4444, name=4444, website=null, bio=null, email=xyritol@kakao.com, phone=null, gender=null, profileImageUrl=null, role=null, createDate=null)
+
+            String rawPassword = user.getPassword();
+            String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+            user.setPassword(encPassword);
+            user.setRole("ROLE_USER");
+            User userEntity = authService.회원가입(user);
+            System.out.println(userEntity);
+
+            return "auth/signin";
+        }
+    }
 }
